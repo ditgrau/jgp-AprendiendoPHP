0b61dedc75fd753f3cd8a53541f961c779908645
DitGrau <juditgraup@gmail.com>
2023-07-11 10:16:11 +0200

    primera ruta: get user

--- app-1/routes/api.php
+++ app-1/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/user/{id}', function($id){
+    $user = DB::table('users')-> where ('id', $id)-> get();
+    return $user;
+});
+
+// get (findAll), find (findOne(pk))
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
