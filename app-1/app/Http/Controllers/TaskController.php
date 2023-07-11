8caad642121d471c6145ea90a2e7829c42d60389
DitGrau <juditgraup@gmail.com>
2023-07-11 10:37:04 +0200

    primer endpoint .get

--- app-1/app/Http/Controllers/TaskController.php
+++ app-1/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends Controller
@@ -16,12 +17,15 @@ class TaskController extends Controller
                 [
                     'message'=> 'Tasks retrieved',
                     'data'=> $tasks
-                ], Response::HTTP_OK
+                ], Response::HTTP_OK //PARA RESPONSE (libreria symfony)
             );
         } catch (\Throwable $th) {
+            Log::error('error getting tasks' . $th->getMessage());
 
+            return response()->json([
+                'message' => 'Error retrieving tasks'
+            ], response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
 
-//PARA RESPONSE (dependencias symfony)
